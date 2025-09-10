95ce74e46130380edfdf6bc8566ebbf54fdf931e
riinakahila <[riina.kahila@myy.haaga-helia.fi]>
2025-09-10 18:21:35 +0300

    lisätty add book ja delete

--- src/main/java/com/harjoitustyo/bookstore/web/BookController.java
+++ src/main/java/com/harjoitustyo/bookstore/web/BookController.java
@@ -1,11 +1,11 @@
 package com.harjoitustyo.bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.harjoitustyo.bookstore.model.Book;
 import com.harjoitustyo.bookstore.model.BookRepository;
@@ -28,12 +28,22 @@ public class BookController {
         return "booklist";
     }
 
-    @GetMapping("add")
+    @GetMapping("/addbook")
     public String openAddBookForm(Model model) {
-        model.addAttribute("kirja", new Book());
-        return "addBook";
+        model.addAttribute("book", new Book());
+        return "addbook";
     }
     
+    @PostMapping("/save")
+    public String saveBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+}
+@GetMapping("/delete/{id}")
+public String deleteBook(@PathVariable("id") Long bookId) {
+    bookRepository.deleteById(bookId);
+    return "redirect:/booklist";
+}
     
 
 }
